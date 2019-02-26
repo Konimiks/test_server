ebf045e4c879b140c1f2de6e3fdb3987675f893f
Konimiks <dyachenko.dmtr@gmail.com>
2019-02-26 12:20:05 +0200

    second

--- server/server.js
+++ server/server.js
@@ -7,6 +7,6 @@ app.use(logger('dev'));
 
 require('./routes').addRoutes(app);
 
-app.listen(8888, function() {
-    console.log(`Server is up and running on port 8888`);
+app.listen(process.env.PORT || 8888, function() {
+    console.log(`Server is up and running on port ` + process.env.PORT);
 });
